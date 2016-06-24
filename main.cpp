157216b125b26919bb4317d8eb5e851fd6d684b2
MobileGameServer <mobilegameserver@gmail.com>
2016-06-25 01:27:30 +0900

    try

--- main.cpp
+++ main.cpp
@@ -8,74 +8,81 @@
 
 int main()
 {
-	mysqlhs::connection c("127.0.0.1", "9999");
-	mysqlhs::index idx(&c, "test", "movie", "PRIMARY", "id,genre,title,view_count");
-
-	bool ret = false;
-	ret = idx.open_();
-	if (!ret)
+	try
 	{
-		std::cout << "failed to open index" << std::endl;
-		return 1;
-	}
+		mysqlhs::connection c("127.0.0.1", "9999");
+		mysqlhs::index idx(&c, "test", "movie", "PRIMARY", "id,genre,title,view_count");
 
-	std::vector<std::string> insert_data;
-	{
-		insert_data.push_back("0");
-		insert_data.push_back("Sci-Fi");
-		insert_data.push_back("Star wars");
-	}
-
-	for (int i = 0; i < 10; ++i)
-	{
-		ret = idx.insert_(insert_data);
+		bool ret = false;
+		ret = idx.open_();
 		if (!ret)
 		{
-			std::cout << "failed to insert" << std::endl;
+			std::cout << "failed to open index" << std::endl;
+			return 1;
 		}
-		std::cout << "insert : " << ret << std::endl;
-	}
 
-	ret = idx.select_where_index('>', 0, 10, 0);
-	if (!ret)
+		std::vector<std::string> insert_data;
+		{
+			insert_data.push_back("0");
+			insert_data.push_back("Sci-Fi");
+			insert_data.push_back("Star wars");
+		}
+
+		for (int i = 0; i < 10; ++i)
+		{
+			ret = idx.insert_(insert_data);
+			if (!ret)
+			{
+				std::cout << "failed to insert" << std::endl;
+			}
+			std::cout << "insert : " << ret << std::endl;
+		}
+
+		ret = idx.select_where_index('>', 0, 10, 0);
+		if (!ret)
+		{
+			std::cout << "failed to select" << std::endl;
+		}
+		std::cout << "select : " << ret << std::endl;
+
+		mysqlhs::result rs(&idx);
+		std::cout << "num_rows : " << rs.num_rows() << std::endl;
+
+		while (rs.fetch())
+		{
+			std::cout << rs.get<int>("id") << std::endl;
+			std::cout << rs.get_str("genre") << std::endl;
+			std::cout << rs.get_str("title") << std::endl;
+			std::cout << rs.get<int>("view_count") << std::endl;
+			std::cout << std::endl;
+		}
+
+		std::vector<std::string> update_data;
+		{
+			update_data.push_back("11");
+			update_data.push_back("Sci-Fi updated");
+			update_data.push_back("Star wars updated");
+			update_data.push_back("101");
+		}
+
+		ret = idx.update_where_index('=', 10, update_data);
+		if (!ret)
+		{
+			std::cout << "failed to update" << std::endl;
+		}
+		std::cout << "update : " << idx.conn()->affected_rows() << std::endl;
+
+		ret = idx.delete_where_index('=', 9);
+		if (!ret)
+		{
+			std::cout << "failed to delete" << std::endl;
+		}
+		std::cout << "delete : " << idx.conn()->affected_rows() << std::endl;
+	}
+	catch (std::exception& e)
 	{
-		std::cout << "failed to select" << std::endl;
+		std::cout << e.what() << std::endl;
 	}
-	std::cout << "select : " << ret << std::endl;
-
-	mysqlhs::result rs(&idx);
-	std::cout << "num_rows : " << rs.num_rows() << std::endl;
-
-	while (rs.fetch())
-	{
-		std::cout << rs.get<int>("id") << std::endl;
-		std::cout << rs.get_str("genre") << std::endl;
-		std::cout << rs.get_str("title") << std::endl;
-		std::cout << rs.get<int>("view_count") << std::endl;
-		std::cout << std::endl;
-	}
-
-	std::vector<std::string> update_data;
-	{
-		update_data.push_back("11");
-		update_data.push_back("Sci-Fi updated");
-		update_data.push_back("Star wars updated");
-		update_data.push_back("101");
-	}
-
-	ret = idx.update_where_index('=', 10, update_data);
-	if (!ret)
-	{
-		std::cout << "failed to update" << std::endl;
-	}
-	std::cout << "update : " << idx.conn()->affected_rows() << std::endl;
-
-	ret = idx.delete_where_index('=', 9);
-	if (!ret)
-	{
-		std::cout << "failed to delete" << std::endl;
-	}
-	std::cout << "delete : " << idx.conn()->affected_rows() << std::endl;
 
 	return 0;
 }
